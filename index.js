dc30801a2a0e56c804f6bf00246db0321cc8dc97
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2023-06-01 08:44:44 +0700

    feat: install swagger

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUiExpress from 'swagger-ui-express'
 
 import routes from './src/routes.js'
+
 dotenv.config()
 
 const app = express()
@@ -36,6 +39,30 @@ app.use(
 routes(app)
 
 app.use(express.json())
+
+// SWAGGER CONFIG
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Tasker API with Swagger',
+      version: '1.0.0',
+      description: 'Tasker API with Swagger',
+      contact: {
+        name: 'Tasker API'
+      },
+      servers: [
+        {
+          url: 'http://localhost:3001'
+        }
+      ]
+    }
+  },
+  apis: ['./src/docs/schemas.js']
+}
+
+const specs = swaggerJsdoc(options)
+app.use('/api-docs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs, { explorer: true }))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
